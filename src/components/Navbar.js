bcd2a82035a2dfaee98108fe9a6b907ba137ac72
mjkatgithub <melchior.kannengiesser@gmx.de>
2020-05-03 18:23:32 +0200

    move navbar to components

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import styles from '../styles.module.css'
+
+const Navbar = ({ links }) => {
+    const linkList = links.map((link) => {
+      return (
+        <li key={link.key}>
+          <a href={link.path}>{link.caption}</a>
+        </li>
+      )
+    })
+    return (
+      <div className={styles.navbar}>
+        <ul>{linkList}</ul>
+      </div>
+    )
+  }
+
+  export default Navbar
--- src/index.js
+++ src/index.js
@@ -1,21 +1,13 @@
 import React from 'react'
 import styles from './styles.module.css'
 
+import Navbar from './components/Navbar'
+
+
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
 
-export const Navbar = ({ links }) => {
-  const linkList = links.map(link => {
-    return (
-      <li key={link.key}><a href={link.path}>{link.caption}</a></li>
-    )
-  })
-  return (
-    <div className={styles.navbar}>
-      <ul>
-        {linkList}
-      </ul>
-    </div>
-  )
-}
+
+
+export default Navbar
