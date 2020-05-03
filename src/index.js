06cdbda0bf8e0e21d2c8ce34015829ef56ee783e
mjkatgithub <melchior.kannengiesser@gmx.de>
2020-05-03 22:29:14 +0200

    green the screen

--- src/index.js
+++ src/index.js
@@ -1,10 +1,3 @@
-import React from 'react'
-//import styles from './styles.module.css'
-
 import Navbar from './components/Navbar'
-
-//export const ExampleComponent = ({ text }) => {
-//  return <div className={styles.test}>Example Component: {text}</div>
-//}
 
 export default Navbar
---
+++ src/index.test.js
@@ -0,0 +1,7 @@
+import Navbar from './components/Navbar'
+
+describe('Navbar', () => {
+  it('is truthy', () => {
+    expect(Navbar).toBeTruthy()
+  })
+})
